5148a613fd25b8b0549735aef2d6bda429f228aa
Ryan Henrick Da Silva Pinto <henrick.s1320@gmail.com>
2024-02-06 15:15:18 -0300

    ImageCard component and gallery page

--- src/app/galeria/page.tsx
+++ src/app/galeria/page.tsx
@@ -1,9 +1,14 @@
-import { Template } from '@/components'
+import { Template , ImageCard} from '@/components'
+
 
 export default function GaleriaPage() {
   return (
     <Template>
-      <h1>Galeria</h1>
+      <section className='grid grid-cols-3 gap-8'>
+      <ImageCard src="https://img.freepik.com/free-photo/wide-angle-shot-single-tree-growing-clouded-sky-during-sunset-surrounded-by-grass_181624-22807.jpg"/>
+      <ImageCard/>
+      <ImageCard/>
+      </section>
     </Template>
   )
 }
