f783925386336e69da1fa1c8ed40e8f791b8c075
Ryan Henrick Da Silva Pinto <henrick.s1320@gmail.com>
2024-02-07 11:35:21 -0300

    testing hook (useState)

--- src/app/galeria/page.tsx
+++ src/app/galeria/page.tsx
@@ -1,24 +1,42 @@
-import { Template , ImageCard} from '@/components'
+'use client'
+
+import { Template, ImageCard } from '@/components';
+import { useState } from 'react';
+
 
 
 export default function GaleriaPage() {
+
+  const image1 = 'https://static.vecteezy.com/system/resources/thumbnails/025/284/015/small_2x/close-up-growing-beautiful-forest-in-glass-ball-and-flying-butterflies-in-nature-outdoors-spring-season-concept-generative-ai-photo.jpg'
+  const image2 = 'https://cdn.pixabay.com/photo/2015/12/01/20/28/road-1072821_640.jpg'
+  const nome1 = 'Laranja'
+  const nome2 = 'Verde'
+
+  const [codigoImage,setCodigoImage] = useState<number>(2);
+  const [urlImage,setUrlImage] = useState<string>(image2); 
+  const [nomeImage,setNomeImage] = useState<string>(nome1); 
+
+
+  function mudarImagem(){
+    if(codigoImage == 1 ){
+      setCodigoImage(2)
+      setUrlImage(image2)
+      setNomeImage(nome1)
+    } else {
+      setCodigoImage(1)
+      setUrlImage(image1)
+      setNomeImage(nome2)
+    }
+
+  }
+
   return (
     <Template>
+      <button className='bg-gray-500' onClick={mudarImagem}>Clique aqui para mudar</button>
       <section className='grid grid-cols-4 gap-8'>
-      <ImageCard  src="https://img.freepik.com/free-photo/wide-angle-shot-single-tree-growing-clouded-sky-during-sunset-surrounded-by-grass_181624-22807.jpg"/>
-      <ImageCard  src="https://img.freepik.com/free-photo/wide-angle-shot-single-tree-growing-clouded-sky-during-sunset-surrounded-by-grass_181624-22807.jpg"/>
-      <ImageCard  src="https://img.freepik.com/free-photo/wide-angle-shot-single-tree-growing-clouded-sky-during-sunset-surrounded-by-grass_181624-22807.jpg"/>
-      <ImageCard  src="https://img.freepik.com/free-photo/wide-angle-shot-single-tree-growing-clouded-sky-during-sunset-surrounded-by-grass_181624-22807.jpg"/>
-      <ImageCard  src="https://img.freepik.com/free-photo/wide-angle-shot-single-tree-growing-clouded-sky-during-sunset-surrounded-by-grass_181624-22807.jpg"/>
-      <ImageCard  src="https://img.freepik.com/free-photo/wide-angle-shot-single-tree-growing-clouded-sky-during-sunset-surrounded-by-grass_181624-22807.jpg"/>
-      <ImageCard  src="https://img.freepik.com/free-photo/wide-angle-shot-single-tree-growing-clouded-sky-during-sunset-surrounded-by-grass_181624-22807.jpg"/>
-      <ImageCard  src="https://img.freepik.com/free-photo/wide-angle-shot-single-tree-growing-clouded-sky-during-sunset-surrounded-by-grass_181624-22807.jpg"/>
-      <ImageCard  src="https://img.freepik.com/free-photo/wide-angle-shot-single-tree-growing-clouded-sky-during-sunset-surrounded-by-grass_181624-22807.jpg"/>
-      <ImageCard  src="https://img.freepik.com/free-photo/wide-angle-shot-single-tree-growing-clouded-sky-during-sunset-surrounded-by-grass_181624-22807.jpg"/>
-      <ImageCard  src="https://img.freepik.com/free-photo/wide-angle-shot-single-tree-growing-clouded-sky-during-sunset-surrounded-by-grass_181624-22807.jpg"/>
-      <ImageCard  src="https://img.freepik.com/free-photo/wide-angle-shot-single-tree-growing-clouded-sky-during-sunset-surrounded-by-grass_181624-22807.jpg"/>
-      <ImageCard  src="https://img.freepik.com/free-photo/wide-angle-shot-single-tree-growing-clouded-sky-during-sunset-surrounded-by-grass_181624-22807.jpg"/>
-      <ImageCard  src="https://img.freepik.com/free-photo/wide-angle-shot-single-tree-growing-clouded-sky-during-sunset-surrounded-by-grass_181624-22807.jpg"/>
+        <ImageCard nome={nomeImage} tamanho="10MB" src={urlImage}/>
+        <ImageCard nome={nomeImage} tamanho="10MB" src={urlImage}/>
+        <ImageCard nome={nomeImage} tamanho="10MB" src={urlImage}/>
       
       </section>
     </Template>
