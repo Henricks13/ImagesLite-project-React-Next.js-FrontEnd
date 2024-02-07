d3611dab6c71abdc26b347200a056ed5a96130c1
Ryan Henrick Da Silva Pinto <henrick.s1320@gmail.com>
2024-02-07 20:58:03 -0300

    Creating the image search form

--- src/app/galeria/page.tsx
+++ src/app/galeria/page.tsx
@@ -18,11 +18,37 @@ export default function GaleriaPage() {
     console.table(result);
   }
 
+  function renderImageCard(image:Image){
+    return(
+        <ImageCard nome={image.name} 
+                   src={image.url} 
+                   tamanho={image.size} 
+                   dataUpload={image.uploadDate} />
+    )
+  }
+
+  function renderImageCards(){
+    return images.map(renderImageCard)
+  }
+  
+
   return (
     <Template>
-      <button className='bg-gray-500' onClick={searchImages}>Clique aqui para mudar</button>
+      <section className='flex flex-col items-center justify-center my-5'>
+        <div className='flex space-x-4'>
+          <input type="text"  className='border px-3 py-2 rounded-lg text-gray-900'/>
+          <select className='border px-4 py-2 rounded-lg text-gray-900' name="" id="">
+            <option value="">All Formats</option>
+          </select>
+          <button className='bg-blue-500 text-white px-4 py-2 rounded-lg' onClick={searchImages}>Search</button>
+          <button className='bg-yellow-500 text-white px-4 py-2 rounded-lg'>Add New</button>
+        </div>
+      </section>
       <section className='grid grid-cols-4 gap-8'>
-        <ImageCard nome="{nomeImage}" tamanho="10MB" src=''/>
+
+        {
+          renderImageCards()
+        }
       
       </section>
     </Template>
