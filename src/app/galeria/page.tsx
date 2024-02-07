b48ed55d419962ac8b3adb8c21ef5d1541a0105b
Ryan Henrick Da Silva Pinto <henrick.s1320@gmail.com>
2024-02-07 14:48:48 -0300

    Bringing data from the BackEnd to the FrontEnd

--- src/app/galeria/page.tsx
+++ src/app/galeria/page.tsx
@@ -1,42 +1,28 @@
 'use client'
 
 import { Template, ImageCard } from '@/components';
+import { Image } from '@/resources/image/image.resource';
+import {useImageService} from '@/resources/image/image.service'
 import { useState } from 'react';
 
 
 
 export default function GaleriaPage() {
 
-  const image1 = 'https://static.vecteezy.com/system/resources/thumbnails/025/284/015/small_2x/close-up-growing-beautiful-forest-in-glass-ball-and-flying-butterflies-in-nature-outdoors-spring-season-concept-generative-ai-photo.jpg'
-  const image2 = 'https://cdn.pixabay.com/photo/2015/12/01/20/28/road-1072821_640.jpg'
-  const nome1 = 'Laranja'
-  const nome2 = 'Verde'
+  const useService = useImageService();
+  const [images, setImages] = useState<Image[]>([])
 
-  const [codigoImage,setCodigoImage] = useState<number>(2);
-  const [urlImage,setUrlImage] = useState<string>(image2); 
-  const [nomeImage,setNomeImage] = useState<string>(nome1); 
-
-
-  function mudarImagem(){
-    if(codigoImage == 1 ){
-      setCodigoImage(2)
-      setUrlImage(image2)
-      setNomeImage(nome1)
-    } else {
-      setCodigoImage(1)
-      setUrlImage(image1)
-      setNomeImage(nome2)
-    }
-
+  async function searchImages(){
+    const result = await useService.buscar()
+    setImages(result);
+    console.table(result);
   }
 
   return (
     <Template>
-      <button className='bg-gray-500' onClick={mudarImagem}>Clique aqui para mudar</button>
+      <button className='bg-gray-500' onClick={searchImages}>Clique aqui para mudar</button>
       <section className='grid grid-cols-4 gap-8'>
-        <ImageCard nome={nomeImage} tamanho="10MB" src={urlImage}/>
-        <ImageCard nome={nomeImage} tamanho="10MB" src={urlImage}/>
-        <ImageCard nome={nomeImage} tamanho="10MB" src={urlImage}/>
+        <ImageCard nome="{nomeImage}" tamanho="10MB" src=''/>
       
       </section>
     </Template>
