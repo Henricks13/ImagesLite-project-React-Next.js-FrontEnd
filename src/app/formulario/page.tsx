a0aa822c36611d4057d6661a37f6f20f10d3d680
Ryan Henrick Da Silva Pinto <henrick.s1320@gmail.com>
2024-02-08 12:56:11 -0300

    Upload sent to the API via form.

--- src/app/formulario/page.tsx
+++ src/app/formulario/page.tsx
@@ -1,5 +1,6 @@
 'use client'
 import { InputText, Template, Button,RenderIf } from "@/components"
+import {useImageService} from '@/resources/image/image.service'
 import Link from "next/link"
 import {useFormik} from 'formik'
 import { useState } from "react";
@@ -16,13 +17,24 @@ const formScheme: FormProps = {name: '', tags: '', file: ''}
 export default function FormularioPage(){
 
   const [imagePreview, setImagePreview] = useState<string>();
+  const service = useImageService();
 
   const formik = useFormik({
     initialValues: formScheme,
-    onSubmit: (dados: FormProps) => {
-      console.log(dados)
-    }
+    onSubmit: handleSubmit
   })
+
+   async function handleSubmit(dados: FormProps){
+    const formData = new FormData();
+    formData.append("file", dados.file)
+    formData.append("name", dados.name)
+    formData.append("tags", dados.tags)
+
+    await service.salvar(formData);
+
+    formik.resetForm();
+    setImagePreview('')
+  }
 
   function onFileUpload(event: React.ChangeEvent<HTMLInputElement>){
     if(event.target.files){
@@ -62,14 +74,14 @@ export default function FormularioPage(){
                           <path fillRule="evenodd" d="M1.5 6a2.25 2.25 0 012.25-2.25h16.5A2.25 2.25 0 0122.5 6v12a2.25 2.25 0 01-2.25 2.25H3.75A2.25 2.25 0 011.5 18V6zM3 16.06V18c0 .414.336.75.75.75h16.5A.75.75 0 0021 18v-1.94l-2.69-2.689a1.5 1.5 0 00-2.12 0l-.88.879.97.97a.75.75 0 11-1.06 1.06l-5.16-5.159a1.5 1.5 0 00-2.12 0L3 16.061zm10.125-7.81a1.125 1.125 0 112.25 0 1.125 1.125 0 01-2.25 0z" clipRule="evenodd" />
                     </svg>
                   </RenderIf>
-                  <div className="mt-4 flex text-sm leading-6 text-gray-600">
+                  <div className="p-5 flex text-sm leading-6 text-gray-600">
                     <label className="relative cursor-pointer rounded-md bg-white font-semibold text-indigo-600">
                       <RenderIf condition={!imagePreview}>
                         <span>Click to upload</span>
                       </RenderIf>
 
                       <RenderIf condition={!!imagePreview}>
-                        <img src={imagePreview} width={250} className="rounded-md" />
+                        <img src={imagePreview} width={250}  className="rounded-md" />
                       </RenderIf>
 
                       <input onChange={onFileUpload} type="file" className="sr-only" />
