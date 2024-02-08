b69d6389f690847eeec34c1644b370062d42f9cd
Ryan Henrick Da Silva Pinto <henrick.s1320@gmail.com>
2024-02-07 21:17:06 -0300

    Image search logic implementation

--- src/app/galeria/page.tsx
+++ src/app/galeria/page.tsx
@@ -11,17 +11,20 @@ export default function GaleriaPage() {
 
   const useService = useImageService();
   const [images, setImages] = useState<Image[]>([])
+  const [query, setQuery] = useState<string>('')
+  const [extension, setExtension] = useState<string>('')
 
   async function searchImages(){
-    const result = await useService.buscar()
+    console.log("valor digitado:", query)
+    const result = await useService.buscar(query,extension)
     setImages(result);
-    console.table(result);
   }
 
   function renderImageCard(image:Image){
     return(
         <ImageCard nome={image.name} 
-                   src={image.url} 
+                   src={image.url}
+                   key={image.url} 
                    tamanho={image.size} 
                    dataUpload={image.uploadDate} />
     )
@@ -36,15 +39,21 @@ export default function GaleriaPage() {
     <Template>
       <section className='flex flex-col items-center justify-center my-5'>
         <div className='flex space-x-4'>
-          <input type="text"  className='border px-3 py-2 rounded-lg text-gray-900'/>
-          <select className='border px-4 py-2 rounded-lg text-gray-900' name="" id="">
+          <input type="text"  
+          onChange={event => setQuery(event.target.value)}
+          className='border px-3 py-2 rounded-lg text-gray-900'/>
+          <select onChange={event => setExtension(event.target.value)} 
+                  className='border px-4 py-2 rounded-lg text-gray-900' name="" id="">
             <option value="">All Formats</option>
+            <option value="PNG">PNG</option>
+            <option value="JPEG">JPEG</option>
+            <option value="GIF">GIF</option>
           </select>
           <button className='bg-blue-500 text-white px-4 py-2 rounded-lg' onClick={searchImages}>Search</button>
           <button className='bg-yellow-500 text-white px-4 py-2 rounded-lg'>Add New</button>
         </div>
       </section>
-      <section className='grid grid-cols-4 gap-8'>
+      <section  className='grid grid-cols-4 gap-8'>
 
         {
           renderImageCards()
