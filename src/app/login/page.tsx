1b6a6425d25ce8dd0ae0ad483749b69740cc2eeb
Ryan Henrick Da Silva Pinto <henrick.s1320@gmail.com>
2024-02-17 18:00:48 -0300

    Applying validations to the login and registration form

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,6 +1,6 @@
 'use client'
 
-import { Template, RenderIf, InputText } from "@/components"
+import { Template, RenderIf, InputText,Button } from "@/components"
 import { useState } from "react"
 
 export default function login(){
@@ -13,12 +13,12 @@ export default function login(){
             <div className="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8">
                 <div className="sm:mx-auto sm:w-full sm:max-w-sm">
                     <h2 className="mt-10 text-center text-1x1 font-bold leading-9 tracling-tight text-gray-900">
-                        Login to your account
+                        { newUserState ? "Created New User" : "Login to Your Account" }
                     </h2>
                 </div>
 
                 <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-1m ">
-                    <form className="space-y-6" action="">
+                    <form className="space-y-2" action="">
                         <RenderIf condition={newUserState}>
                             <div>
                                 <label className="block text-sm dont-medium leading-6 text-gray-900" htmlFor="">Name:</label>
@@ -65,6 +65,31 @@ export default function login(){
                                             
                                 </div>
                             </RenderIf>
+
+                            <div>
+                                <RenderIf condition={newUserState}>
+                                    <Button type="submit" 
+                                            style="bg-indigo-700 hover:bg-indigo-500" 
+                                            label="Save"/>
+
+                                    <Button type="button" 
+                                            style="bg-red-700 hover:bg-red-500 mx-2" 
+                                            label="Cancel"
+                                            onClick={event => setNewUserState(false)}/>
+                                </RenderIf>
+
+                                <RenderIf condition={!newUserState}>
+                                    <Button type="button" 
+                                            style="bg-indigo-700 hover:bg-indigo-500" 
+                                            label="Login"/>
+
+                                    <Button type="button" 
+                                            style="bg-red-700 hover:bg-red-500 mx-2" 
+                                            label="Sign Up" 
+                                            onClick={event => setNewUserState(true)}/>
+                                </RenderIf>
+
+                            </div>
 
                     </form>
                 </div>
